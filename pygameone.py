edd53828c5b0392879d538d810120850347198be
Neves <rafaelnvss002@gmail.com>
2024-06-27 16:41:47 -0300

    multiplos certo

--- pygameone.py
+++ pygameone.py
@@ -19,14 +19,12 @@ pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(2)
 som_tiro = pygame.mixer.Sound("shoot.mp3")
 
-
-
 zombie1 = pygame.image.load("zombie1.png")
 z1_x, z1_y = zombie1.get_size() # ajuste do tamanho do zumbi
 zombie1 = pygame.transform.smoothscale(zombie1, (z1_x*0.3, z1_y*0.3)) #zumbi1
 zombie1 = pygame.transform.flip(zombie1, True, False) # gira em torno do eixo X
 posicao_zombie1 = [1000, 300] #define por onde o zombie1 vai andar horizontalmente
-zombie1_view = True
+
 
 class Zombie:
     def __init__(self, x, y, velocidade):
@@ -35,43 +33,51 @@ class Zombie:
         self.velocidade = velocidade
 
     def update(self): # andar
-        self.rect.x -= 1
+        self.rect.x -= self.velocidade
 
     def draw(self, surface):
         surface.blit(self.imagem, self.rect)
 
 
-def adicionarzumbi():
-    posicoes = [15, 100, 200] # define as posições iniciais dos zombies
-    velocidade = 0.5
-    i = random.randint(0, 2)
-    return Zombie(905, posicoes[i], velocidade)
+def adicionarzumbi(pos, velocidade):
+    return Zombie(905, pos, velocidade)
 
-zumbis = [adicionarzumbi() for _ in range(5)]
+
+inicial_pos = [15, 100, 200]  # posições Y iniciais dos zumbis
+zumbis_normais = []
+
+for i in range(3):
+    zumbis_normais.append(adicionarzumbi(inicial_pos[i], 1))
 
 game = True
 while game:
     clock.tick(30)
     janela.blit(fundo, (0, 0))  # background
     x, y = pygame.mouse.get_pos()
-    pos_cursor = [x-130, y-130]  # centralize o cursor
-    janela.blit(imagem_cursor, pos_cursor)  # atualiza a pos do novo cursor
+    pos_cursor = [x-130, y-130]  # centraliza
+    janela.blit(imagem_cursor, pos_cursor)  # atualiza o novo cursor
 
     for event in pygame.event.get():
-        # imgzombie1_rect = zombie1.get_rect(topleft=posicao_zombie1) uma lista, pega o retângulo, a superficie onde a imagem está
         if event.type == pygame.QUIT:
             game = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
+
             som_tiro.play()
             print(x, y)
             pos_click = event.pos
+            for zumbi in zumbis_normais:
+                print(zumbi.rect.x)
+                area_zumbi = zumbi.imagem.get_rect(topleft=[zumbi.rect.x, zumbi.rect.y])  # verifica se o mouse clicou na área de algum zumbi dentro do loop
+                if area_zumbi.collidepoint(pos_click):
+                    zumbis_normais.remove(zumbi)
+
             # if imgzombie1_rect.collidepoint(pos_click): # se o mouse click clicar no espaço onde a imagem está
                 # zombie1_view = False
 
-    for zumbi in zumbis:
-        zumbi.draw(janela)
-        zumbi.update()
+    for zumbi in zumbis_normais:
+        zumbi.draw(janela)  # movimenta o zumbi
+        zumbi.update()  #
 
-    pygame.display.flip()
+    pygame.display.flip()  # update tela
 
 pygame.quit()
